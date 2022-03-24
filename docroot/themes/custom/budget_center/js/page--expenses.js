78d1b50b213c80126a23ddda9b3d21fe7a69a28f
timhtheos <me@timothyae.com>
2022-03-24 13:15:34 +0800

    Huge update.

--- docroot/themes/custom/budget_center/js/page--expenses.js
+++ docroot/themes/custom/budget_center/js/page--expenses.js
@@ -9,42 +9,111 @@
     attach: function(context, settings) {
       var prefix = "₱ ";
 
-      // Expenses.
-      var page = $(".path-frontpage"),
-        total_amount = 0;
-
-      $(page).find(".view-expenses tbody tr").once("pageExpensesLandingRow").each(function(index_row) {
-        var items_count = 0;
-
-        $(this).once("pageExpensesLandingRowDetails").find(".views-field-field-expense-item li").each(function(index_details) {
-          var units = $(this).find(".field--name-field-no-of-units").text(),
-            unit_price = $(this).find(".field--name-field-unit-cost").text(),
-            total_cost = parseFloat(units)*parseFloat(unit_price);
-          items_count++;
-          total_amount = total_amount + total_cost;
+      // Item expenses.
+      $(document).ready( function () {
+        $(".page--expenses-item table.table").DataTable({
+          autoWidth: false,
+          bSort: false,
+          bLengthChange: false,
+          iDisplayLength: 10,
+          fixedHeader: {
+            header: true,
+          },
+          scrollX:        true,
+          scrollCollapse: true,
+          fixedColumns:   {
+            left: 3,
+            right: 3,
+          },
+          oLanguage: {
+            "sSearch": "Quick Filter Items:"
+          },
+          aoColumns: [
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+            null,
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+          ]
         });
-
-        $(this).find(".views-field-nothing").html(prefix + " " + numberWithCommas((total_amount).toFixed(2)));
-        $(this).find(".views-field-field-expense-item .item-list").after(items_count + " item(s)");
       });
 
-      // Expense details.
-      var page = $(".page-node-type-expense"),
-        total_amount = 0;
-
-      $(page)
-      .find(".attachment-after .view-expense-details tbody tr")
-      .once("pageExpensesDetails")
-      .each(function(index) {
-        var units = $(this).find(".views-field-field-no-of-units").text(),
-          unit_price = ($(this).find(".views-field-field-unit-cost:not('.expense-item-total-cost')").text()).split(" ")[1],
-          total_cost = parseFloat(units)*parseFloat(unit_price);
-
-        total_amount = total_amount + total_cost;
-        $(this).find(".expense-item-total-cost").html(prefix + " " + (total_cost).toFixed(2));
+      // Line expenses.
+      $(document).ready( function () {
+        $(".page--expenses-line table.table").DataTable({
+          autoWidth: false,
+          bSort: false,
+          bLengthChange: false,
+          iDisplayLength: 10,
+          fixedHeader: {
+            header: true,
+          },
+          scrollX:        true,
+          scrollCollapse: true,
+          fixedColumns:   {
+            left: 2,
+            right: 2,
+          },
+          oLanguage: {
+            "sSearch": "Quick Filter Items:"
+          },
+          // bFilter: false,
+          aoColumns: [
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+            {bSearchable: false},
+            null,
+            {bSearchable: false},
+            {bSearchable: false},
+          ]
+        });
       });
 
-      $(page).find(".expense-total-amount").once("pageExpensesDetailsTotal").html(prefix + " " + numberWithCommas((total_amount).toFixed(2)));
+      // // Expenses.
+      // var page = $(".path-frontpage"),
+      //   total_amount = 0;
+
+      // $(page).find(".view-expenses tbody tr").once("pageExpensesLandingRow").each(function(index_row) {
+      //   var items_count = 0;
+
+      //   $(this).once("pageExpensesLandingRowDetails").find(".views-field-field-expense-item li").each(function(index_details) {
+      //     var units = $(this).find(".field--name-field-no-of-units").text(),
+      //       unit_price = $(this).find(".field--name-field-unit-cost").text(),
+      //       total_cost = parseFloat(units)*parseFloat(unit_price);
+      //     items_count++;
+      //     total_amount = total_amount + total_cost;
+      //   });
+
+      //   $(this).find(".views-field-nothing").html(prefix + " " + numberWithCommas((total_amount).toFixed(2)));
+      //   $(this).find(".views-field-field-expense-item .item-list").after(items_count + " item(s)");
+      // });
+
+      // // Expense details.
+      // var page = $(".page-node-type-expense"),
+      //   total_amount = 0;
+
+      // $(page)
+      // .find(".attachment-after .view-expense-details tbody tr")
+      // .once("pageExpensesDetails")
+      // .each(function(index) {
+      //   var units = $(this).find(".views-field-field-no-of-units").text(),
+      //     unit_price = ($(this).find(".views-field-field-unit-cost:not('.expense-item-total-cost')").text()).split(" ")[1],
+      //     total_cost = parseFloat(units)*parseFloat(unit_price);
+
+      //   total_amount = total_amount + total_cost;
+      //   $(this).find(".expense-item-total-cost").html(prefix + " " + (total_cost).toFixed(2));
+      // });
+
+      // $(page).find(".expense-total-amount").once("pageExpensesDetailsTotal").html(prefix + " " + numberWithCommas((total_amount).toFixed(2)));
 
     },
   };
